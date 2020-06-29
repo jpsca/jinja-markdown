00b338bb8d0f8b96d9449de80cb05c7b9708cfbd
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2020-06-29 17:45:33 -0500

    Ta-da

---
+++ test_extension.py
@@ -0,0 +1,30 @@
+from jinja2 import Environment
+from jinja_markdown import MarkdownExtension
+
+
+SOURCE = """
+<article>
+    {% markdown %}
+    # Heading
+    Regular text
+
+        code
+    {% endmarkdown %}
+</article>
+"""
+
+EXPECTED = """
+<article>
+    <h1>Heading</h1>
+<p>Regular text</p>
+<div class="codehilite"><pre><span></span><code><span class="err">code</span>
+</code></pre></div>
+</article>"""
+
+
+def test_render_markdown():
+    jinja_env = Environment(extensions=[MarkdownExtension])
+    tmpl = jinja_env.from_string(SOURCE)
+    output = tmpl.render()
+    print(output)
+    assert output == EXPECTED
